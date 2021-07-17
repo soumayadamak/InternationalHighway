24fa3d8e2642472777464b76067321cee1e3ed37
unknown <alijendoubi521@gmail.com>
2021-07-17 17:17:05 +0100

    Changes done Last time

--- highway-app/src/components/Header.jsx
+++ highway-app/src/components/Header.jsx
@@ -7,31 +7,31 @@ import "./styles/Header.css";
 
 function Heading() {
     return (
-        <nav class="navbar navbar-expand-lg navbar-light bg-light ">
-            <div class="Flex">
-                <a class="" href="#"><img src="./images/logo.png" alt="" class="logo-img"></img></a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon" id="Burger"></span>
+        <nav className="navbar navbar-expand-lg navbar-light ">
+            <div >
+                <a className="" href="#"><img src="./images/logo.png" alt="" className="logo-img"></img></a>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon" id="Burger"></span>
                 </button>
             </div>
 
-            <div class="collapse navbar-collapse" id="navbarNavDropdown">
-                <ul class="navbar-nav">
-                    <li class="nav-item active">
-                        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+            <div className="collapse navbar-collapse" id="navbarNavDropdown">
+                <ul className="navbar-nav">
+                    <li className="nav-item active">
+                        <a className="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
                     </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    <li className="nav-item dropdown">
+                        <a className="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                             Services              </a>
-                        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                            <a class="dropdown-item" href="/SAT">SAT Prep</a>
-                            <a class="dropdown-item" href="/guidance">University Guidance</a>
+                        <div className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                            <a className="dropdown-item" href="/SAT">SAT Prep</a>
+                            <a className="dropdown-item" href="/guidance">University Guidance</a>
                         </div>
                     </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    <li className="nav-item dropdown">
+                        <a className="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                             Opportunities              </a>
-                        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                        <div className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
                             <a class="dropdown-item" href="#">For High School Students</a>
                             <a class="dropdown-item" href="#">For University Students</a>
                         </div>
